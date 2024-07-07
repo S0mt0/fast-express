354ba240c6d916d504767f7b65a3c9152b79fc49
S0mt0 <sewkito@gmail.com>
2024-07-07 09:46:02 +0100

    seo description

--- app/paypal/layout.tsx
+++ app/paypal/layout.tsx
@@ -3,6 +3,8 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "PayPal | A Simple and Safer Way to Pay and Get Paid",
   icons: [{ url: "/images/paypal.png", href: "/images/paypal.png" }],
+  description:
+    "PayPal Holdings, Inc. is an American multinational financial technology company operating an online payments system in the majority of countries that support online money transfers; it serves as an electronic alternative to traditional paper methods such as checks and money orders.",
 };
 
 export default function PayPalLayout({

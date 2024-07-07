5bd09dd4683d54b70842ccbe4c039118eb0f18dc
S0mt0 <sewkito@gmail.com>
2024-07-07 20:44:43 +0100

    update

--- app/paypal/checkout/loader1.tsx
+++ app/paypal/checkout/loader1.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export const Loader1 = () => {
   return (
-    <div className="fixed inset-0 grid place-items-center bg-white/80">
+    <div className="fixed inset-0 grid place-items-center bg-white/80 z-20">
       <Loader2
         className="h-24 w-24 sm:h-32 sm:w-32 text-[#123172] animate-spin"
         strokeWidth={"0.3px"}
